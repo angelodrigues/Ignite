c0e63a1cf188580df315f6470bdef8e421446de4
Angelo Rodrigues <angelorodrigues318@gmail.com>
2023-01-26 11:49:51 -0300

    authentication with github user, using next-auth.

--- reactJs/03-next-minews/src/components/SignInButton/index.tsx
+++ reactJs/03-next-minews/src/components/SignInButton/index.tsx
@@ -1,25 +1,30 @@
 import { FaFacebook, FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 import styles from './styles.module.scss';
 
+
 export function SignInButton() {
-    const isUserLoggedIn = false;
+    const {data: session} = useSession();
 
+    console.log(session)
 
-    return isUserLoggedIn ? (
+    return session ? (
         <button 
             type="button"
             className={styles.signInButton}
+            onClick={() => signOut()}
         >
             <FaFacebook color='#04d361'/>
-            Angelo Barcelos
+            { session.user.name }
             <FiX color='#737380' className={styles.clseIcon} />
         </button>
     ) : (
         <button 
             type="button"
             className={styles.signInButton}
+            onClick={() => signIn('github')}
         >
             <FaFacebook color='#eba417'/>
             Sign in with facebook
