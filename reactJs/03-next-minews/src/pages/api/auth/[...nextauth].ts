21c4b1b24ec6649db51a099ab80b3232dfc7db75
Angelo Rodrigues <angelorodrigues318@gmail.com>
2023-01-26 23:43:42 -0300

    recording user accesses . Checking for duplicates

--- reactJs/03-next-minews/src/pages/api/auth/[...nextauth].ts
+++ reactJs/03-next-minews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from "../../../services/fauna";
+import { query as q } from "faunadb";
 
 export default NextAuth({
   
@@ -14,4 +17,39 @@ export default NextAuth({
       }
     }),
   ],
+  callbacks: {
+    async signIn({user, account, profile}) {
+
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('UserByEmail'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('UserByEmail'),
+                q.Casefold(user.email) 
+              )
+            )
+          )
+        )
+  
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 })
